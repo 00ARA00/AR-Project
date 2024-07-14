0a0ef2fc57201b39b0e31e4fef1cfa9604c50817
00ARA00 <artur2.rasulzade@gmail.com>
2024-07-14 17:38:58 +0500

    First commit
    First commit

---
+++ ARTest/Assets/ProjectFolder/SpawnObjectOnImage.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+[RequireComponent(typeof(ARTrackedImageManager))]
+public class SpawnObjectOnImage : MonoBehaviour
+{
+    [SerializeField] private ARTrackedImageManager _trackedImagesManager;
+    [SerializeField] private GameObject[] ArPrefabs;
+    private readonly Dictionary<string, GameObject> _instantiatedPrefabs = new Dictionary<string, GameObject>();
+
+    void OnEnable()
+    {
+        _trackedImagesManager.trackedImagesChanged += OnTrackedImagesChanged;
+    }
+
+    void OnDisable()
+    {
+        _trackedImagesManager.trackedImagesChanged -= OnTrackedImagesChanged;
+    }
+
+    private void OnTrackedImagesChanged(ARTrackedImagesChangedEventArgs eventArgs)
+    {
+        foreach (var trackedImage in eventArgs.updated)
+        {
+            var imageName = trackedImage.referenceImage.name;
+            foreach (var curPrefab in ArPrefabs)
+            {
+                if (imageName == curPrefab.name)
+                {
+                    var newPrefab = Instantiate(curPrefab, trackedImage.transform);
+                    _instantiatedPrefabs[imageName] = newPrefab;
+                }
+            }
+        }
+
+        foreach (var trackedImage in eventArgs.updated)
+        {
+            _instantiatedPrefabs[trackedImage.referenceImage.name].SetActive(trackedImage.trackingState == TrackingState.Tracking);
+        }
+
+        foreach (var trackedImage in eventArgs.removed)
+        {
+            Destroy(_instantiatedPrefabs[trackedImage.referenceImage.name]);
+            _instantiatedPrefabs.Remove(trackedImage.referenceImage.name);
+        }
+    }
+}
