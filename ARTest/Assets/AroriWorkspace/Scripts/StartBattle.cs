b1e91d8e6ad499741bc0b9b79ee74a9e311e914c
00ARA00 <164861092+00ARA00@users.noreply.github.com>
2024-09-04 08:23:00 +0500

    SkillsUIButtons

---
+++ ARTest/Assets/AroriWorkspace/Scripts/StartBattle.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartBattle : MonoBehaviour
+{
+    [SerializeField] private SpawnInitializer spawnInitializer;
+
+
+
+
+    private void Awake()
+    {
+        
+    }
+}
